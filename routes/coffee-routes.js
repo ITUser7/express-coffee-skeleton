086132a1aa434a60d072be76670686cd504cd391
ITUser7 <365500829@qq.com>
2015-03-03 20:46:54 +0800

    Update coffee-routes.js

--- routes/coffee-routes.js
+++ routes/coffee-routes.js
@@ -4,3 +4,4 @@ exports.findAll = function(req, res){
 exports.findById = function(req, res){
 	res.send({id:req.params.id, name: "The Name", description: "description"});
 };
+exports.addCoffee = function(req, res) {id:req.params.id, name: "The Name", description: "description"}
